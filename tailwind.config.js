86abd90aa379bd7a11a962625bd437e0d8913a68
liannaindia <ind9146279251@gmail.com>
2025-11-02 14:40:15 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        saffron: "#FF9933",
+        lotus: "#E64A19",
+        indigo: "#3F51B5",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
