782a4b6b63b1fa98118faf0b749f1efa48dd7395
liannaindia <ind9146279251@gmail.com>
2025-11-02 17:10:39 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // Tailwind 样式入口
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./components/Home.jsx";
+import Markets from "./components/Markets.jsx";
+import Trade from "./components/Trade.jsx";
+import Positions from "./components/Positions.jsx";
+import Me from "./components/Me.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="markets" element={<Markets />} />
+          <Route path="trade" element={<Trade />} />
+          <Route path="positions" element={<Positions />} />
+          <Route path="me" element={<Me />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
