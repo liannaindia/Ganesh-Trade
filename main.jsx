1f853bdb6913874c3b8632c0f7ba10782a056c09
liannaindia <ind9146279251@gmail.com>
2025-11-03 20:00:52 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,26 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import Home from "./components/Home.jsx";
-import Markets from "./components/Markets.jsx";
-import Trade from "./components/Trade.jsx";
-import Positions from "./components/Positions.jsx";
-import Me from "./components/Me.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="markets" element={<Markets />} />
-          <Route path="trade" element={<Trade />} />
-          <Route path="positions" element={<Positions />} />
-          <Route path="me" element={<Me />} />
-        </Route>
-      </Routes>
-    </Router>
+    <App />  {/* 只需要加载 App.jsx */}
   </React.StrictMode>
 );
