da95e850fdd6b82b8c0e0fa50a558e1eedf6fd3d
liannaindia <ind9146279251@gmail.com>
2025-11-03 23:07:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,        // 使用端口 3000，可以选择 8080
+    host: '0.0.0.0'    // 确保应用监听所有网络接口，允许外部访问
+  }
 })
