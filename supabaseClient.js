d6264beaebcba508d6dbd6189697593f3c07dad1
liannaindia <ind9146279251@gmail.com>
2025-11-04 22:44:42 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 使用您的 Supabase URL 和 API 密钥
-const supabase = createClient(
-  'https://gigzrgapctbdrbmbkcia.supabase.co', // 替换成您的 Supabase 项目 URL
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZ3pyZ2FwY3RiZHJibWJrY2lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNTI2NDIsImV4cCI6MjA3NzcyODY0Mn0.F6dnpa2--Q-xy1mlndbvsmKvvHN1hSKgh-kykCztNwQ' // 替换成您的匿名 API 密钥
-);
+// 在 Supabase Dashboard 获取的 URL 和 API 密钥
+const SUPABASE_URL = 'https://gigzrgapctbdrbmbkcia.supabase.co';  // 替换为你的 Supabase URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZ3pyZ2FwY3RiZHJibWJrY2lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNTI2NDIsImV4cCI6MjA3NzcyODY0Mn0.F6dnpa2--Q-xy1mlndbvsmKvvHN1hSKgh-kykCztNwQ';  // 替换为你的 Supabase anon 密钥
 
-export default supabase;
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
