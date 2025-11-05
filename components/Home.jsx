f1841a2eede8a818cd30913ea8370042394b4320
liannaindia <ind9146279251@gmail.com>
2025-11-05 21:33:26 +0800

    Update Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -202,6 +202,7 @@ export default function Home({ setTab }) {
         isLoggedIn={isLoggedIn}
         balance={balance}
         handleLoginRedirect={handleLoginRedirect}
+        setTab={setTab}
       />
 
       {/* Market Data Section */}
