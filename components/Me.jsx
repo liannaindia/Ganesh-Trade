f59a4d2bd137e24db530348db3e437bba0266545
liannaindia <ind9146279251@gmail.com>
2025-11-11 14:43:07 +0800

    Update Me.jsx

--- components/Me.jsx
+++ components/Me.jsx
@@ -248,7 +248,7 @@ export default function Me({ setTab, userId, isLoggedIn }) {
           {
             icon: <FileText className="h-5 w-5 text-slate-600" />,
             label: "Follow Order",
-            tab: "positions",
+            tab: "followorder",
           },
           {
             icon: <FileText className="h-5 w-5 text-slate-600" />,
